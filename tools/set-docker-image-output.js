c21e0aa1df77277a62f7da6072ca7786b5a6d40b
Ifeanyi Echeruo <ifeanyi.echeruo@outlook.com>
2021-06-25 05:11:56 -0700

    Deploy workflow fix relative path

--- tools/set-docker-image-output.js
+++ tools/set-docker-image-output.js
@@ -1,4 +1,4 @@
-const {name, version} = require('./package.json'); 
+const {name, version} = require('../package.json'); 
 const { argv } = process;
 const [ node, script, ...args ] = argv;
 const [ registry, commitHash ] = args;
